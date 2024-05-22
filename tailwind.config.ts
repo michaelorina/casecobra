d18ebd6a91b07beae7565f4fa0b8795ac8aeaec7
michaelorina <orinamichael4@gmail.com>
2024-05-22 17:09:23 +0300

    casecobra tailwind config update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,13 +59,13 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+        "fade-in": {
+          from: {
+            opacity: "0"
+          },
+          to: {
+            opacity: "1"
+          }
         },
         marquee: {
           '100%': {
@@ -75,8 +75,7 @@ const config = {
       },
       animation: {
         "marquee": 'marquee var(--marquee-duration) linear infinite',
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        "fade-in": "fade-in 0.5s linear forwards"
       },
     },
   },
