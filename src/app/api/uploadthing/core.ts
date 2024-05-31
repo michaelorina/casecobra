59f1f1968409a75d87de999e32a69bec094ddba9
michaelorina <orinamichael4@gmail.com>
2024-05-31 13:04:50 +0300

    casecobra upload configurator logic

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,8 @@
 import { metadata } from '@/app/layout'
 import { createUploadthing, type FileRouter } from 'uploadthing/next'
 import { z } from 'zod'
+import sharp from "sharp"
+import { db } from '@/db'
 
 
 const f = createUploadthing()
@@ -11,10 +13,37 @@ export const ourFileRouter = {
     .middleware(async ({ input }) => {
       return { input }
     })
-    .onUploadComplete(async ({metadata, file}) => {
-      const {configId} = metadata.input
-      return {configId}
+    .onUploadComplete(async ({ metadata, file }) => {
+      const { configId } = metadata.input
+
+      const res = await fetch(file.url)
+      const buffer = await res.arrayBuffer()
+
+      const imgMetadata = await sharp(buffer).metadata()
+      const { width, height } = imgMetadata
+
+      if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500
+          },
+        })
+        return { configId: configuration.id }
+      } else {
+        const updatedConfiguration = await db.configuration.update({
+          where: {
+            id: configId,
+          },
+          data: {
+            croppedImageUrl: file.url
+          },
+        })
+
+        return { configId: updatedConfiguration.id }
+      }
     }),
-  } satisfies FileRouter
+} satisfies FileRouter
 
 export type OurFileRouter = typeof ourFileRouter
